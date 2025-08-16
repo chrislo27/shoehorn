96e04999eb2a4087bf76fe411008df9b1ffb6fef
chrislo27 <kinchrislo@gmail.com>
2025-08-16 11:02:31 -0700

    Add changeable real IP header check

--- webserver/src/main/kotlin/com/swingnosefrog/shoehorn/webserver/util/HeaderUtils.kt
+++ webserver/src/main/kotlin/com/swingnosefrog/shoehorn/webserver/util/HeaderUtils.kt
@@ -5,13 +5,30 @@ import io.javalin.http.Context
 
 object RealIPFromHeader {
     
+    const val HEADER_CLOUDFRONT_CONNECTING_IP: String = "CF-Connecting-IP"
+    const val HEADER_X_REAL_IP: String = "X-Real-IP"
+    const val HEADER_X_FORWARDED_FOR: String = "X-Forwarded-For"
+    
+    var headerCheckOrder: List<String> = listOf(
+        HEADER_CLOUDFRONT_CONNECTING_IP,
+        HEADER_X_REAL_IP,
+        HEADER_X_FORWARDED_FOR,
+    )
+    
+    private fun checkHeadersInOrder(ctx: Context): String? {
+        val headersToCheck = headerCheckOrder
+
+        for (header in headersToCheck) {
+            val headerValue = ctx.header(header)?.takeUnless { it.isBlank() } ?: continue
+            return headerValue
+        }
+        
+        return null
+    }
+    
     fun getRealIPFromHeader(ctx: Context): String {
-        return ctx.header("CF-Connecting-IP")?.takeUnless { it.isBlank() }
-            ?: ctx.header("X-Real-IP")?.takeUnless { it.isBlank() }
-            ?: ctx.header("X-Forwarded-For")?.takeUnless { it.isBlank() }
-            ?: ctx.ip()
+        return checkHeadersInOrder(ctx) ?: ctx.ip()
     }
 }
 
 fun Context.getRealIPFromHeader(): String = RealIPFromHeader.getRealIPFromHeader(this)
-
